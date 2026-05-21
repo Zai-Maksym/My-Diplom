a52f9ac7634d804fba62bf4389c9fa1b02bf4886
Zai-Maksym <cs30112018@gmail.com>
2026-05-21 22:46:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ form.addEventListener("submit", async function(event){
 
     const data = {
 
-        name: document.getElementById("name").value,
+        first_name: document.getElementById("first_name").value,
 
-        surname: document.getElementById("surname").value,
+        last_name: document.getElementById("last_name").value,
 
         email: document.getElementById("email").value,
 
         phone: document.getElementById("phone").value,
 
-        booking_date: document.getElementById("date").value,
+        booking_date: document.getElementById("booking_date").value,
 
-        room_name: "Номер для одного"
+        room_id: 1
 
     };
 
